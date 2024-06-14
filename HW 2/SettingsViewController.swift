f8244982961a4815a7060f781a256e0deecc3e73
MaratXcode <kosjachok8@yandex.ru>
2024-06-14 21:31:42 +0300

    worc and prepea data

--- HW 2/SettingsViewController.swift
+++ HW 2/SettingsViewController.swift
@@ -12,15 +12,26 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
-    @IBOutlet weak var colorView: UIView!
-
-    @IBOutlet weak var redLabel: UILabel!
-    @IBOutlet weak var greenLabel: UILabel!
-    @IBOutlet weak var blueLabel: UILabel!
     
-    @IBOutlet weak var redSlider: UISlider!
-    @IBOutlet weak var greenSlider: UISlider!
+    
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    
+    @IBOutlet weak var blueTF: UITextField!
+    @IBOutlet weak var greenTF: UITextField!
+    @IBOutlet weak var redTF: UITextField!
+    
+    
+    
+    
+    @IBOutlet var colorView: UIView!
     
     var delegate: SettingsViewControllerDelegate!
     
@@ -32,10 +43,48 @@ class SettingsViewController: UIViewController {
         
         colorView.layer.cornerRadius = 15
         
-        redSlider.minimumTrackTintColor = .red
-        greenSlider.minimumTrackTintColor = .green
         colorView.backgroundColor = colorFromSuper
+        getValuesFromView()
+        presentSliderValuesInTextFieldAndLabels()
     }
+    
+    
+    @IBAction func rgbSlidersTriggered() {
+        
+        colorView.backgroundColor = UIColor( red: CGFloat((redSlider.value)),
+                                             green: CGFloat(greenSlider.value),
+                                             blue: CGFloat(blueSlider.value),
+                                             alpha: 1 )
+        presentSliderValuesInTextFieldAndLabels()
+    }
+    
+    func presentSliderValuesInTextFieldAndLabels() {
+        
+        redTF.text = String(redSlider.value)
+        greenTF.text = String(greenSlider.value)
+        blueTF.text = String(blueSlider.value)
+        
+        
+    }
+    
+    func getValuesFromView() {
+        guard let color = colorView.backgroundColor else { return }
+        let ciColor = CIColor(color: color)
+        redSlider.value = Float(ciColor.red)
+        greenSlider.value = Float(ciColor.green)
+        blueSlider.value = Float(ciColor.blue)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func doneButtonPressed() {
        
